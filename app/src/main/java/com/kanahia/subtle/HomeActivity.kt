d00b5e7469adf4e11709d6fc73be3a00c25a68c9
Kanahia <kaushalkanahia@gmail.com>
2024-08-08 23:30:40 +0530

    Added home page

--- app/src/main/java/com/kanahia/subtle/HomeActivity.kt
+++ app/src/main/java/com/kanahia/subtle/HomeActivity.kt
@@ -1,27 +1,18 @@
 package com.kanahia.subtle
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kanahia.subtle.databinding.ActivityHomeBinding
 import com.kanahia.subtle.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2000)
     }
-
 }
